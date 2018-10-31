b4ba4eeba6efc2fbef1d5c06ca73caa6b0705297
Elsa Szymczak <elsaszymczak@gmail.com>
2018-10-31 18:19:20 +0800

    edit post and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :set_post, only: [:show, :edit, :update, :detroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all.order('created_at desc')
@@ -17,21 +17,25 @@ class PostsController < ApplicationController
    @post = Post.new(post_params)
    if @post.save
     redirect_to posts_path
-  else
-    render :new
-  end
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @post.destroy
+    redirect_to root_path
   end
 
   private
